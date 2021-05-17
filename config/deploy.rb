1639682e51c939ec01963ad84bad70dd30dd06b8
GhaniyKie <abdulhakimghaniy@outlook.co.id>
2021-05-17 18:03:05 +0700

    Revert "Forward to Previous"
    This reverts commit 2aef3505018f29281fcd836bf0c1fed61efeb225.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,14 +1,14 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.16.0"
 
-set :application, "skidi-backend"
-set :repo_url, "git@github.com:Skidi-2021/skidi-backend-api.git"
+set :application, "my_app_name"
+set :repo_url, "git@example.com:me/my_repo.git"
 
 # Default branch is :master
-ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-set :deploy_to, "/home/skidi-backend/#{fetch :application}"
+# set :deploy_to, "/var/www/my_app_name"
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
@@ -24,7 +24,7 @@ set :deploy_to, "/home/skidi-backend/#{fetch :application}"
 # append :linked_files, "config/database.yml"
 
 # Default value for linked_dirs is []
-append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", 'vendor/bundle', '.bundle', 'public/system', 'public/uploads'
+# append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
@@ -33,7 +33,7 @@ append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", 'vendor/bund
 # set :local_user, -> { `git config user.name`.chomp }
 
 # Default value for keep_releases is 5
-set :keep_releases, 5
+# set :keep_releases, 5
 
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure

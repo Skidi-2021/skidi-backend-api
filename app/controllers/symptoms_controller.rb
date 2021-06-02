23fba3d7f6b6c5eb8b2dc78a696eed29d2371a32
GhaniyKie <abdulhakimghaniy@outlook.co.id>
2021-06-02 13:26:58 +0700

    Menambahkan beberapa fungsionalitas di Controller Symptoms

--- app/controllers/symptoms_controller.rb
+++ app/controllers/symptoms_controller.rb
@@ -1,15 +1,43 @@
 class SymptomsController < ApplicationController
+    before_action :authenticate_user!
+    
+    include Helpers::SymptomHelper
+
+    def index
+        symptoms = Symptom.where(user_id: params[:user_id])
+
+        if current_user.id == params[:user_id].to_i
+            render json: SymptomSerializer.new(symptoms)
+        else
+            render json: { error: '401 Unauthorized' }, status: 401
+        end
+
+
+    end
 
     def show
         symptom = Symptom.find(params[:id])
-        render json: show_symptom(symptom), status: 200
+
+        options = {
+            include: [:user]
+        }
+
+        if current_user.id == params[:user_id].to_i
+            render json: SymptomSerializer.new(symptom, options)
+        else
+            render json: { error: '401 Unauthorized' }, status: 401
+        end
     end
-    
+
     def create
-        symptom = Symptom.new(create_params)
-        
+        symptom = Symptom.new(symptom_create)
+
         if symptom.save
-            render json: "Sukses", status: 200
+            begin
+                render json: symptom_response(symptom.latitude, symptom.longitude, symptom.symptom_name), status: 200
+            rescue RestClient::Exceptions::OpenTimeout
+                render json: { error: "Request Timeout" }, status: 408
+            end
         else
             render json: "Gagal", status: :unprocessable_entity
         end
@@ -27,16 +55,27 @@ class SymptomsController < ApplicationController
 
     private
 
-    def create_params
-        params.require(:symptom).permit(:name, :image)
+    def symptom_index(symptoms)
+    end
+
+    def symptom_create
+        {
+            image: params[:image],
+            symptom_name: params[:symptom_name],
+            author: current_user.full_name,
+            latitude: params[:latitude],
+            longitude: params[:longitude],
+            user_id: current_user.id
+        }
     end
     
     def show_symptom(symptom)
         {
             symptom: {
-                name: symptom.name,
-                author: "Ghaniy",
-                url: rails_blob_url(symptom.image)
+                id: symptom.id,
+                symptom_name: symptom.symptom_name,
+                author: symptom.author,
+                url: rails_service_blob_path(filename: symptom.image.filename, signed_id: symptom.image.signed_id)
             }
         }
     end

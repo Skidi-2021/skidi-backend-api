36ac8f1de34e960a47d628506759d124fcf3ef49
GhaniyKie <abdulhakimghaniy@outlook.co.id>
2021-06-02 13:33:17 +0700

    Menambahkan Routes untuk Devise Authentication dan menambahkan routes lainnya untuk Users dan Symptoms

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,20 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  scope :api, defaults: { format: :json } do
-    resources :symptoms, only: [:show, :create] do
-      get :image, on: :member
+  scope :api, defaults: { format: :json } do 
+    devise_scope :user do
+      devise_for :users,   only: []
+      post   'register',   to: 'auth/registrations#create'
+      post   'login',      to: 'auth/sessions#create'
+      delete 'logout',     to: 'auth/sessions#destroy'
+      put    'credential', to: 'auth/credentials#update'
+    end
+    # _ END OF DEVISE _ #
+
+    resources :users do
+      resources :symptoms, only: [:index, :show, :new, :create] do
+        get :image, on: :member
+      end  
     end
   end
 end

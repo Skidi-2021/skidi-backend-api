8a5883e60c627fe64e7b655265481ccf197fdccb
GhaniyKie <abdulhakimghaniy@outlook.co.id>
2021-06-02 13:25:09 +0700

    Menambahkan migration untuk JwtDenylist, dan Devise. Serta menambahkan beberapa atribut untuk Symptoms

--- db/migrate/20210517065925_create_symptoms.rb
+++ db/migrate/20210517065925_create_symptoms.rb
@@ -1,8 +1,14 @@
 class CreateSymptoms < ActiveRecord::Migration[6.1]
   def change
     create_table :symptoms do |t|
-      t.string :name
-      t.string :author
+      t.string   :symptom_name
+      t.string   :author
+      
+      # GeoLocation for Medical Purpose
+      t.float    :latitude
+      t.float    :longitude
+
+      t.belongs_to :user
 
       t.timestamps
     end
